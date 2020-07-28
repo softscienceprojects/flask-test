3eee4b7e4a0dd3dc2c1333e68edcc61ab40932d0
erin <erin.nicole.johnson@gmail.com>
2020-07-28 13:40:47 +0100

    Update myprojecttest.py

--- myprojecttest.py
+++ myprojecttest.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
         return "<h1 style='color:blue'>Hello world</h1>"
 
+@app.route("/db")
+def test_db():
+        return "<h1>{}</h1>".format(os.environ.get("DATABSE_URL"))
+
 if __name__ == "__main__":
         app.run(host='0.0.0.0')
